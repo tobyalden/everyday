ce0642873c8d39554bb17ca62b75ddf8df267a1d
Toby Alden <toby.alden@gmail.com>
2018-07-20 17:40:19 -0700

    Finish laser beams

--- src/entities/LaserBeam.hx
+++ src/entities/LaserBeam.hx
@@ -22,16 +22,25 @@ class LaserBeam extends Entity
         setHitboxTo(graphic);
         moveTo(laser.centerX - 2, laser.centerY - 2);
         laser.type = "notwalls";
-        var blockerX:Float = 4;
         if(laser.direction == "right") {
             moveTo(HXP.width, y, "walls", true);
-            blockerX = x + width;
+            var blockerX = x + width;
             moveTo(laser.centerX - 2, laser.centerY - 2);
             graphic = new ColoredRect(blockerX - x, 4, 0xFF0000);
         }
         else if(laser.direction == "left") {
             moveTo(0, y, "walls", true);
             graphic = new ColoredRect(laser.centerX - x, 4, 0xFF0000);
+        }
+        else if(laser.direction == "down") {
+            moveTo(x, HXP.height, "walls", true);
+            var blockerY = y + height;
+            moveTo(laser.centerX - 2, laser.centerY - 2);
+            graphic = new ColoredRect(4, blockerY - y, 0xFF0000);
+        }
+        else if(laser.direction == "up") {
+            moveTo(x, 0, "walls", true);
+            graphic = new ColoredRect(4, laser.centerY - y, 0xFF0000);
         }
         setHitboxTo(graphic);
         laser.type = "walls";
