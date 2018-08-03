f87faca2ceaf432f781adda6fe1db03c47aa807d
Toby Alden <toby.alden@gmail.com>
2018-08-03 12:30:24 -0700

    Add file picker

--- src/entities/BuildModeUI.hx
+++ src/entities/BuildModeUI.hx
@@ -1,12 +1,17 @@
 package entities;
 
+import flash.events.Event;
+import flash.net.FileReference;
+import flash.net.FileFilter;
 import haxepunk.*;
 import haxepunk.graphics.*;
 import haxepunk.graphics.text.*;
+import haxepunk.input.*;
 
 class BuildModeUI extends Entity
 {
     private var onIndicator:Text;
+    private var fileRef:FileReference;
 
     public function new()
     {
@@ -17,6 +22,47 @@ class BuildModeUI extends Entity
         addGraphic(onIndicator);
         graphic.scrollX = 0;
         graphic.scrollY = 0;
+        Key.define("load", [Key.L]);
+    }
+
+    override public function update() {
+        if(!visible) {
+            super.update();
+            return;
+        }
+        if(Input.pressed("load")) {
+            pickScreen();
+        }
+        super.update();
+    }
+
+    private function pickScreen() {
+        fileRef = new FileReference();
+        fileRef.addEventListener(Event.SELECT, onFileSelected);
+        var oelTypeFilter:FileFilter = new FileFilter(".oel Files", "*.oel");
+        fileRef.browse([oelTypeFilter]);
+    }
+    
+    private function onFileSelected(_:Event) {
+        var levelDir = Sys.getCwd().split("bin")[0] + "assets/levels/";
+        findScreen(levelDir, fileRef.name);
+    }
+
+    private function findScreen(directory:String, name:String) {
+        if(sys.FileSystem.exists(directory)) {
+            for(file in sys.FileSystem.readDirectory(directory)) {
+                var path = haxe.io.Path.join([directory, file]);
+                if(!sys.FileSystem.isDirectory(path)) {
+                    if(file == name) {
+                        trace("file found: " + path);
+                    }
+                    // do something with file
+                }
+                else {
+                    var directory = haxe.io.Path.addTrailingSlash(path);
+                    findScreen(directory, name);
+                }
+            }
+        }
     }
 }
-    
